9df327b53daf8881e4b3dfa1880c56c1f742a81e
knblomquist00 <blo18004@byui.edu>
2023-09-25 13:25:44 -0600

    Add project files.

---
+++ angle.cpp
@@ -0,0 +1,21 @@
+/***********************************************************************
+ * Source: https://prod.liveshare.vsengsaas.visualstudio.com/join?F3805C671AA2825CD9AA5EF914BA94164BDA
+ *    Angle 
+ * Summary:
+ *    Just call the Angle unit tests
+ * Author:
+ *    James Helfrich
+ ************************************************************************/
+
+#include "testAngle.h"
+
+/************************************
+ * MAIN
+ * Simple driver
+ ***********************************/
+int main()
+{
+   TestAngle().run();
+
+   return 0;
+}
